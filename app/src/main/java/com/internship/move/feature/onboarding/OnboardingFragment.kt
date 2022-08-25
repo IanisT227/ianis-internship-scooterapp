331020c8de97bf8b4b066d0ca5e18720bc8e3bea
Ianis_Teja <ianist2001@gmail.com>
2022-08-25 12:41:50 +0300

    Changed some of the OnboardingFragment.kt logic

--- app/src/main/java/com/internship/move/feature/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/internship/move/feature/onboarding/OnboardingFragment.kt
@@ -27,9 +27,9 @@ class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
 
     private fun initNextButton() {
         binding.nextItemBtn.setOnClickListener {
-            if (binding.onboardingVP2.currentItem < 3) {
+            if (binding.onboardingVP2.currentItem < binding.onboardingVP2.adapter?.itemCount?.minus(1)!!) {
                 println("Item no" + binding.onboardingVP2.currentItem)
-                if (binding.onboardingVP2.currentItem == 2)
+                if (binding.onboardingVP2.currentItem == binding.onboardingVP2.adapter?.itemCount?.minus(2)!!)
                     binding.nextItemBtn.text = "Get started"
                 binding.onboardingVP2.currentItem = binding.onboardingVP2.currentItem + 1
             } else {
@@ -40,10 +40,46 @@ class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
 
     private fun initList(): List<OnboardingItem> {
         val onboardingItemsList = mutableListOf<OnboardingItem>()
-        onboardingItemsList.add(OnboardingItem(R.drawable.onboarding_1, "Title1", "Body1", true))
-        onboardingItemsList.add(OnboardingItem(R.drawable.onboarding_2, "Title2", "Body2", true))
-        onboardingItemsList.add(OnboardingItem(R.drawable.onboarding_3, "Title3", "Body3", true))
-        onboardingItemsList.add(OnboardingItem(R.drawable.onboarding_4, "Title4", "Body4", false))
+        onboardingItemsList.add(
+            OnboardingItem(
+                R.drawable.onboarding_1,
+                "Safety",
+                "Please wear a helmet and protect yourself while riding",
+                true
+            )
+        )
+        onboardingItemsList.add(
+            OnboardingItem(
+                R.drawable.onboarding_2,
+                "Scan",
+                "Scan the QR code or NFC sticker on top of the scooter to unlock and ride.",
+                true
+            )
+        )
+        onboardingItemsList.add(
+            OnboardingItem(
+                R.drawable.onboarding_3,
+                "Ride",
+                "Step on the scooter with one foot and kick off the ground. When the scooter starts to coast, push the right throttle to accelerate.",
+                true
+            )
+        )
+        onboardingItemsList.add(
+            OnboardingItem(
+                R.drawable.onboarding_4,
+                "Parking",
+                "If convenient, park at a bike rack. If not, park close to the edge of the sidewalk closest to the street. Do not block sidewalks, doors or ramps.",
+                true
+            )
+        )
+        onboardingItemsList.add(
+            OnboardingItem(
+                R.drawable.onboarding_5,
+                "Rules",
+                "You must be 18 years or and older with a valid driving licence to operate a scooter. Please follow all street signs, signals and markings, and obey local traffic laws.",
+                false
+            )
+        )
 
         return onboardingItemsList
     }
