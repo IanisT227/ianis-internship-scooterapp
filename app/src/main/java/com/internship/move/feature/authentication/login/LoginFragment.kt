323f576c3a3b295cd9b75726740033358a2cd232
Ianis_Teja <ianist2001@gmail.com>
2022-08-30 15:26:41 +0300

    Finally the styling works

--- app/src/main/java/com/internship/move/feature/authentication/login/LoginFragment.kt
+++ app/src/main/java/com/internship/move/feature/authentication/login/LoginFragment.kt
@@ -1,7 +1,13 @@
 package com.internship.move.feature.authentication.login
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.internship.move.R
@@ -23,18 +29,30 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         binding.launchHomeBtn.setOnClickListener {
             findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToMapFragment())
         }
-        binding.forgotPasswordTV.addClickableText(text = getString(R.string.forgot_password))
-        {
+        binding.forgotPasswordTV.addClickableText(text = getString(R.string.forgot_password)) {
             findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment())
         }
-        binding.goToRegisterTV.addClickableText(getString(R.string.goToRegisterLink))
-        {
+        binding.goToRegisterTV.addClickableText(getString(R.string.goToRegisterLink)) {
             findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment())
+        }
+
+        val editTexts = listOf(binding.emailInputET, binding.passwordInputET)
+        for (editText in editTexts) {
+            editText.doOnTextChanged { _, _, _, _ ->
+                if (binding.emailInputET.text?.isNotEmpty() == true && binding.passwordInputET.text?.isNotEmpty() == true) {
+                    binding.launchHomeBtn.isEnabled = true
+                    binding.launchHomeBtn.setTextColor(resources.getColor(R.color.neutral_white))
+                } else {
+                    binding.launchHomeBtn.isEnabled = false
+                    binding.launchHomeBtn.setTextColor(resources.getColor(R.color.neutral_pink))
+                }
+            }
         }
     }
 
     private fun initViews() {
         binding.forgotPasswordTV.text = getString(R.string.forgot_password)
         binding.goToRegisterTV.text = getString(R.string.goToRegisterText) + " " + getString(R.string.goToRegisterLink)
+
     }
 }
